440f7dadba450c82c9247a1c836781b03eca115d
yoonhe <yhehe228@gmail.com>
2021-09-01 15:46:32 +0900

    Feat : Todo List 렌더링 구현
    - redux store에 저장된 todoList를 가져와 렌더링합니다

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,21 @@
-import { useEffect, FC } from 'react';
+import { FC } from 'react';
 import { Global } from '@emotion/react';
 import axios from 'axios';
 
-import requestGetTodos from './api/todos';
-import { BASE_URL } from './api/constants/url';
-
-import reset from './styles/reset';
-
-import { worker } from './mockServer/browser';
+import { worker } from 'mockServer/browser';
+import { BASE_URL } from 'api/constants/url';
+import { TodoList } from 'pages/todos';
+import reset from 'styles/reset';
 
 worker.start();
 
 axios.defaults.baseURL = BASE_URL;
 
 const App: FC = () => {
-  const getTodo = async () => {
-    const todos = await requestGetTodos();
-    console.log({ todos });
-  };
-
-  useEffect(() => {
-    getTodo();
-  }, []);
-
   return (
     <>
       <Global styles={reset} />
-      <div>TODO</div>
+      <TodoList />
     </>
   );
 };
