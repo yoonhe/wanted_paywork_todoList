e5af84379795a6961130f109edb3125b653bf8e7
yoonhe <yhehe228@gmail.com>
2021-09-01 21:54:07 +0900

    Feat : Todo 체크를 위한 /todo/:id api 생성

--- src/mockServer/todoListHandlers.ts
+++ src/mockServer/todoListHandlers.ts
@@ -1,4 +1,5 @@
 import { rest } from 'msw';
+import { setLocalStorage } from 'utils/localStorage';
 import { BASE_URL } from '../api/constants/url';
 
 export const todoListHandlers = [
@@ -9,4 +10,38 @@ export const todoListHandlers = [
 
     return res(ctx.status(200), ctx.json(ParseTodos));
   }),
+  rest.post<PostCheckTodoRequestBody>(
+    `${BASE_URL}/todo/:id`,
+    (req, res, ctx) => {
+      const { isCheck } = req.body;
+      const { id } = req.params;
+
+      const todos = localStorage.getItem('todos');
+      const parseTodos: ITodos = todos ? JSON.parse(todos) : [];
+
+      const todoList = parseTodos.todoList.map((todo: ITodo) => {
+        return todo.id === id
+          ? {
+              ...todo,
+              isCheck,
+            }
+          : todo;
+      });
+
+      const updateTodos = {
+        ...parseTodos,
+        todoList,
+      };
+
+      setLocalStorage({ key: 'todos', value: updateTodos });
+
+      return res(
+        ctx.status(200),
+        ctx.json({
+          msg: 'ok',
+          updateTodos,
+        }),
+      );
+    },
+  ),
 ];
